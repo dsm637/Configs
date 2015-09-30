bc9ac98663171907e15601b54a69a14762a0b2c9
dmitropolskiy <dmitropolskiy@industrialdefender.com>
2015-09-30 11:22:34 -0400

    1

---
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,26 @@
+$global:admin = "$"
+$currentUser = [system.security.principal.windowsidentity]::GetCurrent()
+$principal = new-object System.Security.Principal.WindowsPrincipal($currentUser)
+if($principal.IsInRole("Administrators"))
+{
+  $admin = "#"
+}
+
+[system.consolecolor]$global:OldColor = "White"
+
+function prompt
+{
+  $random = new-object random
+  $color = [system.consolecolor]$random.next(10, 16)
+  
+  while($global:oldColor -eq $color)
+  {
+    $color = [system.consolecolor]$random.next(10, 16)
+  }
+
+  $global:oldColor = $color
+
+  $host.ui.rawui.windowtitle = "$($global:admin) $(Get-Location)" 
+  write-host "[$(Get-Date)]>" -nonewline -foregroundcolor $color
+  " "
+}
